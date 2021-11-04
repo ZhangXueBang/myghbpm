d83aefa2bff9b29ac775651d67cd4bb3dfd59be7
miaomiao1993 <1274069178@qq.com>
2021-11-04 17:05:22 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import torch
+import torch
+print(torch.cuda.is_available())
+print(torch.backends.cudnn.is_available())
+print(torch.cuda_version)
+print(torch.backends.cudnn.version())
+git clone https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix
+cd pytorch-CycleGAN-and-pix2pix
